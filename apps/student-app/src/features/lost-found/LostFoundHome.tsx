454e701c44ffa94413b289abf31c1cd10035dad3
Isindu Yuwaneka Wijesinghe <isinduyuwaneka@gmail.com>
2026-02-23 12:52:03 +0530

    Student-app ready for development

---
+++ apps/student-app/src/features/lost-found/LostFoundHome.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function LostFoundHome() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 18 }}>Lost & Found</Text>
+      <Text>Sashini develops this module</Text>
+    </View>
+  );
+}
